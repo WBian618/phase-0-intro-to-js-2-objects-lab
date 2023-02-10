d97392048ea99172706c8325cbf3e0eae518fbb8
webster <wbian618@gmail.com>
2023-02-09 23:30:56 -0500

    commit tree b75527a18610595e1a3028d586b542ace856a017

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 let employee = {
-    name: "",
-    streetAddress: ""
+    name: "A",
+    streetAddress: "2"
 }
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign({}, employee, { [key]: value })
-}
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value
-    return employee
-}
-
-function deleteFromEmployeeByKey(employee, key) {
-    const newObject = Object.assign({}, employee)
-    delete newObject[key]
-    return newObject
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key]
-    return employee
+    const newemployee= { ...employee};
+    newemployee[key] = value;
+    return newemployee;
 
 }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign({}, employee, { [key]: value })
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee[key] = value
+//     return employee
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newObject = Object.assign({}, employee)
+//     delete newObject[key]
+//     return newObject
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key]
+//     return employee
+
+// }
